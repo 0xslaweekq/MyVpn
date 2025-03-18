621a2a79f9d1c1cff688da51854bae79d2fe39ab
Slaweekq <celesik@gmail.com>
2025-03-18 13:45:05 +0700

    1

--- utils/cursor/update-cursor.sh
+++ utils/cursor/update-cursor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo curl -L https://downloader.cursor.sh/linux/appImage/x64 -o /opt/cursor/cursor.AppImage
-sudo chmod +x /opt/cursor/cursor.AppImage
+sudo curl -L https://downloader.cursor.sh/linux/appImage/x64 -o /opt/cursor/cursor.appimage
+sudo chmod +x /opt/cursor/cursor.appimage
