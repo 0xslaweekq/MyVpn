8e1776eafe0799b9b4053eaf61e6f864ec1d3bac
Slaweekq <celesik@gmail.com>
2025-04-14 13:00:53 +0700

    1

--- utils/optimisation.sh
+++ utils/optimisation.sh
@@ -67,7 +67,7 @@ sudo /etc/init.d/tlp restart
 sudo tlp start
 
 echo "🔹 TLP battery charge control configuration complete"
-echo "🔹 Current battery thresholds: Start at 30%, Stop at 97%"
+echo "🔹 Current battery thresholds: Start at 37%, Stop at 91%"
 echo "🔹 You can check battery status with: sudo tlp-stat -b"
 
 echo 'GOVERNOR="performance"' | sudo tee /etc/default/cpufrequtils
